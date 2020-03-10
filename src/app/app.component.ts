488b2191fe5126bc4ea02c93f21b5bda9ef6576e
tony_ojeda <tojeda96@gmai.com>
2020-03-09 20:36:52 -0500

    project ahorcado: dinamic data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ahorcado';
+  palabra = 'CUA';
+  palabraOculta = '';
+  letras = ['A','B','C','D','E','F','G','G','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V',
+'T','U','V','W','X','Y','Z'];
+  constructor() {
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
 }
