893bc6a844d45bcaab7387c524b1991fa1b29d29
tony_ojeda <tojeda96@gmai.com>
2020-03-09 21:23:31 -0500

    project ahorcado: game <3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  palabra = 'CUA';
+  palabra = 'MANGOSTINOS';
   palabraOculta = '';
+  intentos = 0;
+  gano = false;
+  perdio = false;
   letras = ['A','B','C','D','E','F','G','G','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V',
 'T','U','V','W','X','Y','Z'];
   constructor() {
     this.palabraOculta = '_ '.repeat(this.palabra.length);
   }
-}
+
+  comprobar(letra) {
+    this.existeLetra(letra);
+    let palabraOcultaArr = this.palabraOculta.split(' ');
+    
+    for(let i = 0; i < this.palabra.length; i++) {
+      if(this.palabra[i] == letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+  }
+
+  verificaGane() {
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join(' ');
+    if( palabraEvaluar == this. palabra) {
+      this.gano = true;
+      console.log('Usuario Gano');
+    }
+
+    if( this.intentos >= 9) {
+      this.perdio = true;
+      console.log('Usuario perdio');
+    }
+  }
+
+  existeLetra(letra) {
+    if(this.palabra.indexOf(letra) >= 0) {
+      // console.log('Letra existe');
+    } else {
+      // console.log('Letra no existe');
+      this.intentos ++;
+    }
+  }
+
+} 
